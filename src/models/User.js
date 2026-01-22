a10cc49e6ccfa153a9726c13908265d8e183658e
mayankvyas1306 <vyas79303@gmail.com>
2026-01-22 17:01:36 +0530

    password hashed

--- src/models/User.js
+++ src/models/User.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
     fullName:{
@@ -37,6 +38,17 @@ const userSchema = new mongoose.Schema({
         default:"user"
     },
 },{timestamps:true});
+
+
+//Hash password before saving 
+userSchema.pre("save",async function (next){
+    if(!this.isModified("password")) return next();
+
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+
+});
 
 
 //compare password method
