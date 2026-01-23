2ee40cce3e594901d7fafdb41f6ea9c1ce70138a
mayankvyas1306 <vyas79303@gmail.com>
2026-01-23 18:40:01 +0530

    get,post,update,delete by id routes added

--- src/controllers/post.controller.js
+++ src/controllers/post.controller.js
@@ -1,5 +1,6 @@
 const Post = require("../models/Post");
 
+//create a new interview experience post
 const createPost = async(req,res,next)=>{
     try{
         const { companyName, role, tags, difficulty, result,rounds }=req.body;
@@ -24,4 +25,154 @@ const createPost = async(req,res,next)=>{
     }
 };
 
-module.exports = {createPost};
+// get all posts (search + filters + pagination)
+const getAllPosts = async (req,res,next)=>{
+    try{
+        /**
+         * /api/posts?page=1&limit=5&company=amazon&difficulty=Hard&tag=DSA
+         */
+
+        //pagination default
+        const page = Number(req.query.page) || 1;
+        const limit = Number(req.query.limit) || 10;
+        const skip = (page-1)*limit;
+
+        //Filters object (MongoDB query)
+        const filters = {};
+
+        //Search by Company name (case-insensitive)
+        if(req.query.company){
+            filters.companyName = { $regex: req.query.company, $options: "i" };
+        }
+
+        //filter by role
+        if(req.query.role){
+            filters.role = { $regex: req.query, $options: "i"};
+        }
+
+        //filter by difficulty
+        if(req.query.difficulty){
+            filters.difficulty = req.query.difficulty;
+        }
+
+        //filter by result
+        if(req.query.result){
+            filters.result = req.query.result;
+        }
+
+        //filter by tag(single tag for now)
+        if(req.query.tag){
+            filters.tags = {$in:[req.query.tag]};
+        }
+
+        //sort options
+        //latest = newest first
+        //top = most upvotes first
+        let sortOption = { createdAt : -1};
+
+        if(req.query.sort === "top"){
+            sortOption = { upvotesCount: -1, createdAt: -1};
+        }
+
+        //fetch posts from DB
+        const posts = await Post.find(filters)
+            .populate("authorId", "fullName email college year") //show author details
+            .sort(sortOption)
+            .skip(skip)
+            .limit(limit);
+
+        //Total count for frontend pagination UI
+        const totalPosts = await Post.countDocuments(filters);
+
+        res.json({
+            page,
+            limit,
+            totalPosts,
+            totalPages: Math.ceil(totalPosts / limit),
+            posts,
+        });
+    }catch(err){
+        next(err);
+    }
+}
+
+//get post By Id
+const getPostById = async (req,res,next) => {
+    try{
+        const post = await Post.findById(req.params.id).populate(
+            "authorId",
+            "fullName email college year"
+        );
+
+        if(!post){
+            res.status(404);
+            throw new Error("Post not Found");
+        }
+
+        res.json({post});
+    }catch(err){
+        next(err);
+    }
+}
+
+//update Post
+const updatePost = async (req,res,next) => {
+    try{
+        const { companyName, role, tags, difficulty, result, rounds } =req.body;
+
+        const post =await Post.findById(req.params.id);
+
+        if(!post){
+            res.status(404);
+            throw new Error("Post not found");
+        }
+
+        //ownership check (only author can edit)
+        if(post.authorId.toString() !== req.user._id.toString()){
+            res.status(403);
+            throw new Error("You are not allowed to update this post");
+        }
+
+        //company only if values are sent
+        post.companyName = companyName || post.companyName;
+        post.role = role || post.role;
+        post.tags = tags || post.tags;
+        post.difficulty = difficulty || post.difficulty;
+        post.result = result || post.result;
+        post.rounds = rounds || post.rounds;
+
+        const updatePost = await post.save();
+
+        res.json({
+            message : "Post updated successfully",
+            post: updatePost,
+        });
+      }catch(err){
+        next(err);
+      }
+};
+
+const deletePost = async (req,res,next)=>{
+    try{
+        const post = await Post.findById(req.params.id);
+
+        if(!post){
+            res.status(404);
+            throw new Error("Post not Found");
+        }
+
+        //ownership check
+        if(post.authorId.toString() !==req.user._id.toString()){
+            res.status(403);
+            throw new Error("You are not allowed to delete this post");
+        }
+
+        await Post.deleteOne({_id: post._id});
+
+        res.json({message: "Post deleted Successfully "});
+    }catch(err){
+        next(err);
+    }
+};
+
+module.exports = {deletePost,updatePost,createPost, getAllPosts,getPostById};
--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -1,9 +1,19 @@
 const express = require("express");
+
+
 const { protect } = require("../middlewares/auth.middleware");
-const { createPost } = require("../controllers/post.controller");
+const { createPost, getAllPosts, deletePost, updatePost, getPostById } = require("../controllers/post.controller");
 const router = express.Router();
 
+//get all posts (public)
+router.get("/",getAllPosts);
 
+//create post (protected)
 router.post("/",protect,createPost);
+
+//single post routes
+router.get("/:id",getPostById);
+router.put("/:id",protect,updatePost);
+router.delete("/:id",protect,deletePost);
 
 module.exports = router;
