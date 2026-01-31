1cfd38a03c923123c6c6247bea68e13634e2e4fb
mayankvyas1306 <vyas79303@gmail.com>
2026-01-31 23:44:48 +0530

    error fixed in admin route

--- server/src/routes/admin.routes.js
+++ server/src/routes/admin.routes.js
@@ -1,7 +1,7 @@
 const { isAdmin } = require("../middlewares/admin.middleware");
 const { adminDeletePost, getAllPostsAdmin } = require("../controllers/admin.controller");
 const { protect } = require("../middlewares/auth.middleware");
-const express = require('express');
+const express = require("express");
 
 const router=express.Router();
 
@@ -9,3 +9,5 @@ const router=express.Router();
 //admin route
 router.delete("/:id",protect,isAdmin,adminDeletePost);
 router.get("/posts",protect,isAdmin,getAllPostsAdmin);
+
+module.exports= router;
