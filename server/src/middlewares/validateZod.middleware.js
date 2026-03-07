9b5afa2a51fb9ffa3b7c4cf156c6e9f9ca6e77c3
mayankvyas1306 <vyas79303@gmail.com>
2026-03-07 23:55:26 +0530

    fix

--- server/src/middlewares/validateZod.middleware.js
+++ server/src/middlewares/validateZod.middleware.js
@@ -1,16 +1,17 @@
 const AppError = require("../utils/AppError");
 
 const validate = (schema) => (req, res, next) => {
-    try {
-        schema.parse(req.body);
-        next();
-    } catch (error) {
-        if (error.errors) {
-            const errorMessages = error.errors.map((err) => err.message).join(" | ");
-            return next(new AppError(errorMessages, 400));
-        }
-        next(error);
+  try {
+    schema.parse(req.body);
+    next();
+  } catch (error) {
+    if (error.name === 'ZodError' || error.issues) {
+      const errors = error.issues || error.errors;
+      const errorMessages = errors.map((err) => err.message).join(" | ");
+      return next(new AppError(errorMessages, 400));
     }
+    next(error);
+  }
 };
 
 module.exports = validate;
